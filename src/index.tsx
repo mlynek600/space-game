6397bc9e96357a4025e81b83b2c6840c33fe69ac
Adam <adam.wiktor.mlynarczyk@gmail.com>
2021-01-25 17:07:15 +0100

    clearing

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,12 @@ import 'fontsource-roboto'
 
 import App from './components/App/App'
 import ContextProvider from './context'
-import reportWebVitals from './reportWebVitals'
 
 import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </React.StrictMode>,
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
   document.getElementById('root')
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
